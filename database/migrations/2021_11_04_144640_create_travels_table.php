8de0409ae34d07054cdfe67e335185ad04545837
DavideZattra <d.zattra@gmail.com>
2021-11-04 16:28:03 +0100

    migrated table 'travel'

--- database/migrations/2021_11_04_144640_create_travels_table.php
+++ database/migrations/2021_11_04_144640_create_travels_table.php
@@ -13,8 +13,17 @@ class CreateTravelsTable extends Migration
      */
     public function up()
     {
-        Schema::create('travels', function (Blueprint $table) {
+        Schema::create('travel', function (Blueprint $table) {
             $table->id();
+            $table->string('commercial_name', 50);
+            $table->string('country', 20);
+            $table->string('region', 20);
+            $table->string('city', 20);
+            $table->string('period', 20);
+            $table->string('description', 200);
+            $table->float('price', 11, 2)->unsigned();
+            $table->boolean('all_inclusive');
+            $table->smallInteger('seats')->unsigned();
             $table->timestamps();
         });
     }
