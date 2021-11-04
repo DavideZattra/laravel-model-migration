08982e75e602412bc7cbf4a714d848fab942fa28
DavideZattra <d.zattra@gmail.com>
2021-11-04 15:19:28 +0100

    create Travel model and modify HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Travel;
+
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -9,6 +11,6 @@ class HomeController extends Controller
     public function index(){
 
         return view('home');
-        
+
     }
 }
